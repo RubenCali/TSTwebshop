9f3ce58238fc117cbcb27202a9ad6a0f0751180c
RubenCali <ruben.cali.2003@gmail.com>
2021-01-26 10:05:53 +0100

    jasmine begin

---
+++ spec/myspecs.spec.js
@@ -0,0 +1,32 @@
+
+function ready() {
+    let removeCartItemButtons = document.getElementsByClassName('btn-danger')
+    for (let i = 0; i < removeCartItemButtons.length; i++) {
+        let button = removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
+
+    let quantityInputs = document.getElementsByClassName('cart-quantity-input')
+    for (let i = 0; i < quantityInputs.length; i++) {
+        let input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged)
+    }
+
+    let addToCartButtons = document.getElementsByClassName('shop-item-button')
+    for (let i = 0; i < addToCartButtons.length; i++) {
+        let button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
+    }
+
+    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+}
+
+
+describe('Browser tests', function() {
+    it('should have the correct title', function () {
+        expect(document.title).toBe('JASMINE TESTS');
+    });
+    it('should have characterset UTF-8', function () {
+        expect(document.charset).toBe('UTF-8');
+    });
+});
